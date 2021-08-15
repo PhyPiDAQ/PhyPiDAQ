e9d1bdf571af5935c2f13a1204e7106c8c2a553a
Alexander Kaschta <alexander.kaschta@t-online.de>
2021-08-15 12:51:52 +0200

    Updated installation concept

--- installlibs.sh
+++ installlibs.sh
@@ -4,22 +4,29 @@
 #
 # -----------------------------------------------
 
-sudo apt-get install python3-yaml
-sudo apt-get install python3-scipy
-sudo apt-get install python3-matplotlib
-sudo apt-get install python3-pyqt5
-sudo apt-get install libatlas-base-dev # needed to build nupmy
+sudo apt-get install python3-yaml --yes
+sudo apt-get install python3-scipy --yes
+sudo apt-get install python3-matplotlib --yes
+sudo apt-get install python3-pyqt5 --yes
+sudo apt-get install libatlas-base-dev --yes # needed to build numpy
 
-# some python packages for IO
-sudo pip3 install i2cdev
-sudo pip3 install spidev
-sudo pip3 install pyusb
-sudo pip3 install smbus2
+read -p "Do you want to install the sensor drivers (Y/n)?" -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  # some python packages for IO
+  sudo pip3 install i2cdev
+  sudo pip3 install spidev
+  sudo pip3 install pyusb
+  sudo pip3 install smbus2
 
-sudo pip3 install installlibs/whl/*.whl # python wheels
+  sudo pip3 install installlibs/whl/*.whl # python wheels
 
-sudo pip3 install installlibs/tgz/*.tar.gz # python packages 
+  sudo pip3 install installlibs/tgz/*.tar.gz # python packages
 
-sudo dpkg -i installlibs/picoscopelibs/*.deb # picoscope 
-sudo usermod -a -G tty pi # grant acces to USB for user pi
+  # Install all sensor drivers specified in the
+  sudo pip3 install -r /requirements.txt
+
+  sudo dpkg -i installlibs/picoscopelibs/*.deb # picoscope
+  sudo usermod -a -G tty pi # grant access to USB for user pi
+fi
 
