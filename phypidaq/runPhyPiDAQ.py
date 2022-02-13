ad887e1fe5fb55c7f6c45c6ac88f6c039e0ce5b1
Guenter Quast <guenter.quast@online.de>
2022-02-13 16:17:29 +0100

    updated executable part of runPhyPiDAQ: start with python3 -m phypidaq.runPhyPiDAQ <par>

--- phypidaq/runPhyPiDAQ.py
+++ phypidaq/runPhyPiDAQ.py
@@ -589,6 +589,26 @@ class runPhyPiDAQ(object):
 # execute only if called directly, but not when imported
 if __name__ == "__main__":  # - - - - - - - - - - - - - - - - - - - - - -
 
-    daq = runPhyPiDAQ()
+    from .helpers import keyboard_wait
+
+    if len(sys.argv) != 2:
+        print("\n!!! run_phypi.py usage:\n" + 10 * ' ' + "run_phypi.py <config>.daq\n")
+        prompt = "    starting demo mode from configuration PhyPiDemo.daq" \
+                 + "\n" + 25 * ' ' + "type <ret> to continue, 'E+<ret>' to exit -> "
+        answer = keyboard_wait(prompt)
+        if answer == '':
+            sys.argv.append("PhyPiDemo.daq")
+        else:
+            print("     exiting")
+            sys.exit(1)
+
+    daq = runPhyPiDAQ(verbose=1)
+    # 0: only errors are printed
+    # 1: normal output
+    # 2: verbose output
+
     daq.setup()
+    print("DAQ set-up:\n", yaml.dump(daq.PhyPiConfDict))
+
     daq.run()
+
