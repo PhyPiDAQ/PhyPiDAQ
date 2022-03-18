0604a3b6319360afbbfc795b22e5a546e7e08baa
Alexander Kaschta <alexander.kaschta@t-online.de>
2022-03-18 17:48:58 +0100

    Fixed module import

--- phypidaq/__init__.py
+++ phypidaq/__init__.py
@@ -28,4 +28,5 @@ __all__ = ["helpers", "Display", "DataLogger", "DataRecorder", "DataGraphs",
            "HX711Config", "MAX31865Config", "DS18B20Config", "INA219Config",
            "MAX31855Config", "BMPx80Config", "MMA8451Config", "VL53LxConfig",
            "TCS34725Config", "AS7262Config", "AS7265xConfig", "GDK101Config",
-           "MLX90393Config", "BME280Config", "BMP280Config"]
+           "MLX90393Config", "BME280Config", "BMP280Config", "BME680Config",
+           "BMP388Config"]
