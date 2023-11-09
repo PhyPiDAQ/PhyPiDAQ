8a18073ebc5dc9022f5a037a0bedf8aff9b4d391
Guenter Quast <guenter.quast@online.de>
2023-11-09 08:36:23 +0100

    include RC102 and DisplayPoissonEvent in list of modules

--- phypidaq/__init__.py
+++ phypidaq/__init__.py
@@ -23,10 +23,11 @@ __version__ += _version_suffix
 
 # Import components to be callable at package level
 __all__ = ["helpers", "Display", "DataLogger", "DataRecorder", "DataGraphs",
-           "DataSpectrum", "ReplayConfig", "ToyDataConfig",
-           "PSConfig", "MCP3x08Config", "ADS1115Config", "groveADCConfig", "GPIOCount",
+           "DataSpectrum", "ReplayConfig", "ToyDataConfig", "PSConfig",
+           "MCP3x08Config", "ADS1115Config", "groveADCConfig", "GPIOCount",
            "HX711Config", "MAX31865Config", "DS18B20Config", "INA219Config",
            "MAX31855Config", "BMPx80Config", "MMA8451Config", "VL53LxConfig",
            "TCS34725Config", "AS7262Config", "AS7265xConfig", "GDK101Config",
            "MLX90393Config", "BME280Config", "BMP280Config", "BME680Config",
-           "BMP388Config", "TSL45315Config", "RC10xConfig"]
+           "BMP388Config", "TSL45315Config", "RC10xConfig" , "DataSpectrum",
+           "DisplayPoissonEvent", ]
