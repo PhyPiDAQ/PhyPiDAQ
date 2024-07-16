49d076377c90e0cf343753a13e9b92e92597e763
Guenter Quast <guenter.quast@kit.edu>
2024-07-16 23:16:01 +0200

    ruff formatting

--- phypidaq/DisplayPoissonEvent.py
+++ phypidaq/DisplayPoissonEvent.py
@@ -4,12 +4,12 @@ from scipy.special import loggamma
 from scipy.optimize import newton
 import matplotlib as mpl
 
-mpl.use('Qt5Agg')
+mpl.use("Qt5Agg")
 
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
-plt.style.use('dark_background')  # dark canvas background
+plt.style.use("dark_background")  # dark canvas background
 
 
 # helper function (fromPhyPraKit.phyFit)
@@ -69,14 +69,14 @@ class DisplayPoissonEvent:
         self.tflash = max(self.tau / 20, 0.050)  # flash time for object indicating event occurance
         #       self.flash_color = '#ffff80' # yellowish
         self.title_color = "goldenrod"
-        self.flash_color = '#AFCFFF'  # blueish
-        self.bg_color = 'black'
+        self.flash_color = "#AFCFFF"  # blueish
+        self.bg_color = "black"
 
         # initialize an (interactive) figure
         self.fig = plt.figure("PoissonFlash 𝜏=%.3g" % (self.tau), figsize=(6.0, 8.0))
-        self.fig.canvas.mpl_connect('close_event', self.on_mpl_window_closed)
+        self.fig.canvas.mpl_connect("close_event", self.on_mpl_window_closed)
         self.mpl_active = True
-        self.fig.suptitle('Poisson Statistics', size='x-large', color=self.title_color)
+        self.fig.suptitle("Poisson Statistics", size="x-large", color=self.title_color)
         self.fig.subplots_adjust(left=0.1, bottom=0.1, right=0.95, top=0.95, wspace=None, hspace=0.15)
         gs = self.fig.add_gridspec(nrows=4, ncols=1)
         # 1st graph for "Poisson Flash"
@@ -96,8 +96,8 @@ class DisplayPoissonEvent:
         self.flashobj = self.ax.add_patch(c2)  # body of detector
         self.flashobj = self.ax.add_patch(c1)  # border
         self.flashobj = self.ax.add_patch(c)  # light-emitting survface
-        _ = self.ax.text(0.05, 0.96, "Event Rate: %.2f Hz" % (self.rate), transform=self.ax.transAxes, color='yellow')
-        self.txt = self.ax.text(0.05, 0.92, " ", transform=self.ax.transAxes, color='azure')
+        _ = self.ax.text(0.05, 0.96, "Event Rate: %.2f Hz" % (self.rate), transform=self.ax.transAxes, color="yellow")
+        self.txt = self.ax.text(0.05, 0.92, " ", transform=self.ax.transAxes, color="azure")
         # 2nd graph for rate history
         self.axrate = self.fig.add_subplot(gs[-1, :])
         self.axrate.set_ylabel("Counts")
@@ -118,29 +118,29 @@ class DisplayPoissonEvent:
             0.88,
             "$\Delta$t=%2gs  $\Rightarrow$  mean=%.2f" % (self.mean / self.rate, self.mean),
             transform=self.axrate.transAxes,
-            color='orangered',
+            color="orangered",
         )
-        self.axrate.axhline(self.mean, color='red', linestyle='dashed')
+        self.axrate.axhline(self.mean, color="red", linestyle="dashed")
         if self.mean > 1.5:
             z_value = 1.0
             cm1, cp1 = Poisson_CI(self.mean, sigma=z_value)  # 68%  confidence intervall
-            rect1 = patches.Rectangle((self.xmin, cm1), self.xmax - self.xmin, cp1 - cm1, color='green', alpha=0.5)
+            rect1 = patches.Rectangle((self.xmin, cm1), self.xmax - self.xmin, cp1 - cm1, color="green", alpha=0.5)
             self.axrate.add_patch(rect1)
         if self.mean > 10:
             cm2, cp2 = Poisson_CI(self.mean, 2.0)  # 96%  confidence intervall
-            rect2 = patches.Rectangle((self.xmin, cm2), self.xmax - self.xmin, cp2 - cm2, color='yellow', alpha=0.5)
+            rect2 = patches.Rectangle((self.xmin, cm2), self.xmax - self.xmin, cp2 - cm2, color="yellow", alpha=0.5)
             self.axrate.add_patch(rect2)
 
         self.xplt = np.linspace(-self.Npoints * self.interval, 0.0, self.Npoints)
         self.counts = np.zeros(self.Npoints) - 1.0
-        (self.hline,) = self.axrate.plot(self.xplt, self.counts, '.--', lw=1, markersize=6, color="ivory", mec="orange")
+        (self.hline,) = self.axrate.plot(self.xplt, self.counts, ".--", lw=1, markersize=6, color="ivory", mec="orange")
         plt.ion()
         plt.show()
 
     def on_mpl_window_closed(self, ax):
         # detect when matplotlib window is closed
         self.mpl_active = False
-        
+
     def __call__(self):
         """Flash object"""
         N = 0
@@ -158,7 +158,7 @@ class DisplayPoissonEvent:
                 # collect statistics
                 N += 1
                 rate = N / t if N > 2 else 0.0
-                status_text = "active %.1f s" % t + 15 * ' ' + "counts %d" % N + 5 * ' ' + "average rate %.3f Hz" % rate
+                status_text = "active %.1f s" % t + 15 * " " + "counts %d" % N + 5 * " " + "average rate %.3f Hz" % rate
                 # display statistics and make flash object invisible
                 self.txt.set_text(status_text)
                 self.flashobj.set_facecolor(self.bg_color)
@@ -173,7 +173,14 @@ class DisplayPoissonEvent:
                 self.counts[hbin] += 1
             else:
                 print("\n*==* Poissson Flasher exiting ...")
-                print(8 * ' ' + "active %.1f s" % t_last + 5 * ' ' + "counts %d" % N + 5 * ' ' + "average rate %.3f Hz" % rate)
+                print(
+                    8 * " "
+                    + "active %.1f s" % t_last
+                    + 5 * " "
+                    + "counts %d" % N
+                    + 5 * " "
+                    + "average rate %.3f Hz" % rate
+                )
 
 
 def showFlash(mpQ, rate, mean):
