3adc6df268e659b2026fa1af8b9e658280a7e88b
GuenterQuast <Guenter.Quast@online.de>
2024-08-02 13:55:02 +0200

    added argparse

--- examples/scGammaDetector.py
+++ examples/scGammaDetector.py
@@ -2,11 +2,15 @@
 
 """script scGammaDetektor.py
 
-read data from soundcard and disply random events and wave form data
+read data from soundcard and
+ - display oscillogram of raw wave forms
+ - disply individual events as flash and show wave form data (if available)
+ - store time stamps in file
 
-application example for class SoundCardOsci
+This is an application example for the class phypidaq.SoundCardOsci.
 """
 
+import argparse
 import time
 import numpy as np
 import threading
@@ -25,52 +29,114 @@ def showFlash(mpQ, interval):
     flasher()
 
 
-def runOsci():
+def runDAQ():
+    """run data acquistion as thread
+    """
     t_start = time.time()
     t_lastupd = t_start
     osc_wait_time = 0.1
     maxADC = np.float32(display_range)
     while active:
         try:
-            _d = scO()  # get data
+            # get data
+            _d = scO()
             if _d is None:  #
                 return
             count, trg_idx, data = _d
             now = time.time()
-            # extract data of size 100 around trigger
-            if trg_idx is not None:
-                i0 = max(trg_idx - 25, 0)
-                i1 = min(trg_idx + 75, len(data[0]))
-                d = i1 - i0
-                if d < 100:
-                    if i0 == 0:
-                        i1 += 100 - d
-                    else:
-                        i0 -= 100 - d
-                flasherQ.put((now - t_start, data[0][i0:i1] / maxADC))
-            else:
-                flasherQ.put(now - t_start)
-            if Display is not None:
+            # save to file
+            if csvfile is not None:
+                csvfile.write(count, str(now))
+            # show oscillogram of raw wave form
+            if oscDisplay is not None:
                 if (now - t_lastupd) > osc_wait_time:
-                    Display(data, trg_idx)  # show subset of data
+                    oscDisplay(data, trg_idx)  # show subset of data
                     t_lastupd = now
+            # show events
+            if showevents:
+                # extract data of size 100 around trigger
+                if trg_idx is not None:
+                    i0 = max(trg_idx - 25, 0)
+                    i1 = min(trg_idx + 75, len(data[0]))
+                    d = i1 - i0
+                    if d < 100:
+                        if i0 == 0:
+                            i1 += 100 - d
+                        else:
+                            i0 -= 100 - d
+                    flasherQ.put((now - t_start, data[0][i0:i1] / maxADC))
+                else:
+                    flasherQ.put(now - t_start)
         except Exception:
             return
 
 
-# set parameters
-sampling_rate = 192000  # 44100, 48000, 96000 or 192000
-sample_size = 250
-channels = 1  # 1 or 2
+# parse command line arguments
+parser = argparse.ArgumentParser(
+    description="Read waveforms from soundcard and display and optionally store data"
+)
+parser.add_argument(
+    "-q", "--quiet", action="store_true", help="no status output to terminal"
+)
+parser.add_argument(
+    "-o", "--oscilloscope", action="store_true", help="oscilloscope display"
+)
+parser.add_argument(
+    "-n", "--noeventdisplay", action="store_true", help="deactivate event display"
+)
+parser.add_argument(
+    "-f", "--file", type=str, default="", help="base filename to store results"
+)
+parser.add_argument("-t", "--time", type=int, default=3600, help="run time in seconds")
+#
+parser.add_argument(
+    "-s",
+    "--samplingrate",
+    type=int,
+    choices={44100, 48000, 96000, 192000},
+    default=96000,
+    help="sampling rate",
+)
+parser.add_argument(
+    "-c", "--channels", type=int, choices={1, 2}, default=1, help="number of channels"
+)
+parser.add_argument(
+    "-l", "--trglevel", type=float, default=5000, help="level of trigger"
+)
+parser.add_argument(
+    "--trgfalling", action="store_true", help="trigger falling edge"
+)
+parser.add_argument(
+    "-d", "--trgdeactivate", action="store_true", help="deactivate triggering"
+)
+parser.add_argument(
+    "-z", "--samplesize", type=int, default=256, help="number of samples per read"
+)
+parser.add_argument("-r", "--range", type=float, default=2**14, help="display range")
+#
+parser.add_argument(
+    "-i", "--interval", type=float, default=30.0, help="time bin for rate display"
+)
+#
+args = parser.parse_args()
+# - parameters to control the scrpt
+quiet = args.quiet
+osc_display = args.oscilloscope
+showevents = not args.noeventdisplay
+filename = args.file
+run_seconds = args.time
+# - set-up of class phyipdaq.SoundCardOsci
+sampling_rate = args.samplingrate
+sample_size = args.samplesize
+channels = args.channels
 display_range = 2**14  # maximum is 2**15 for 16bit sound card
-run_seconds = 36000  # run-time in seconds
-trgThreshold = 5500  # for CERN DIY particle detector
-trgFalling = False
-trgActive = True
-interval = 60
-osc_display = True
+trgThreshold = args.trglevel
+trgFalling = args.trgfalling
+trgActive = not args.trgdeactivate
+# - parameter for DisplayPoissonEvent
+interval = args.interval
 
-# create a configuration dictionary
+# create a configuration dictionary for SoundCardOsci
 confd = {
     "sampling_rate": sampling_rate,
     "number_of_samples": sample_size,
@@ -80,6 +146,14 @@ confd = {
     "trgThreshold": trgThreshold,
     "trgFalling": trgFalling,
 }
+
+csvfile = None
+if filename != "":
+    timestamp = time.strftime("%y%m%d-%H%M", time.localtime())
+    fn = filename + "_" + timestamp + ".csv"
+    csvfile = open(fn, "w")
+    csvfile.write("event_times[s]\n")
+
 
 # background process to show event in real-time as "flash"
 flasherQ = mp.Queue()
@@ -95,10 +169,10 @@ scO = SoundCardOsci(confdict=confd)
 scO.init()
 # process to read oscilloscope and display wave forms
 if osc_display:
-    Display = scOsciDisplay(confdict=confd)
+    oscDisplay = scOsciDisplay(confdict=confd)
 else:
-    Display = None
-osciThread = threading.Thread(target=runOsci, args=(), daemon=True)
+    oscDisplay = None
+osciThread = threading.Thread(target=runDAQ, args=(), daemon=True)
 
 # start data acquisition loop
 wait_time = 1.0
@@ -138,3 +212,5 @@ finally:
     flasherProc.terminate()
     time.sleep(1.0)
     scO.close()
+    if csvfile is not None:
+        csvfile.close()
