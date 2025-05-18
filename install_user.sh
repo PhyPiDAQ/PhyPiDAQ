b81f8fe4290bd5281298f549c0e3dbc87fde5046
Guenter Quast <guenter.quast@online.de>
2025-05-18 11:27:28 +0200

    icon installation optional

--- install_user.sh
+++ install_user.sh
@@ -25,24 +25,10 @@ mkdir -p $DIR
 if [ -d $DIR ]; then
 # enter here, if directory exists
 
-  # Create desktop icons dynamically
-  printf "[Desktop Entry]\nType=Application\nEncoding=UTF-8\nName=PhyPi\nComment=PhyPi Configuration\nIcon=" > $HOME/Desktop/phypi.desktop
-  printf $DIR >> $HOME/Desktop/phypi.desktop
-  printf "/PhiPi_icon.png\nExec=lxterminal -t \"PhyPiDAQ\" -e \"" >> $HOME/Desktop/phypi.desktop
-  printf "source ~/activate_phypy.sh; phypi.py\"\nTerminal=false\n" >> $HOME/Desktop/phypi.desktop
 
-  printf "[Desktop Entry]\nType=Application\nEncoding=UTF-8\nName=PhyPi Demo\nComment=PhyPi Configuration\nIcon=" > $HOME/Desktop/phypi_demo.desktop
-  printf $DIR >> $HOME/Desktop/phypi_demo.desktop
-  printf "/PhiPi_icon.png\nExec=lxterminal -t \"PhyPiDAQ\" -e \"" >> $HOME/Desktop/phypi_demo.desktop
-  printf "source ~/activate_phypy.sh; phypi.py " >> $HOME/Desktop/phypi_demo.desktop
-  printf $DIR >> $HOME/Desktop/phypi_demo.desktop
-  printf "/PhyPiDemo.daq\"\nTerminal=false\n" >> $HOME/Desktop/phypi_demo.desktop
-
-  # Ensure, that the desktop links have the correct permissions
-  chmod a+x $HOME/Desktop/*.desktop
-
+	  
   # copy script to activate virtual Python environment
-  cp -auv activate_phypy.sh $HOME
+  cp -auv activate_phypi.sh $HOME
   
   #copy python code
   cp -auv phypi.py $DIR
@@ -55,6 +41,33 @@ if [ -d $DIR ]; then
 
   # copy examples
   cp -auv examples/ $DIR
+
+  # optionally, install Dekstop icons for Raspberry pi
+  while true; do
+    read -p "Do you wish to install desktop icons (only for Raspberry pi) (y/n)? " yn
+    case $yn in
+        [Yy]* ) echo "Installing Desktop Icons";
+
+          # Create desktop icons dynamically
+          printf "[Desktop Entry]\nType=Application\nEncoding=UTF-8\nName=PhyPi\nComment=PhyPi Configuration\nIcon=" > $HOME/Desktop/phypi.desktop
+          printf $DIR >> $HOME/Desktop/phypi.desktop
+          printf "/PhiPi_icon.png\nExec=lxterminal -t \"PhyPiDAQ\" -e \"" >> $HOME/Desktop/phypi.desktop
+          printf "source ~/activate_phypi.sh; phypi.py\"\nTerminal=false\n" >> $HOME/Desktop/phypi.desktop
+
+          printf "[Desktop Entry]\nType=Application\nEncoding=UTF-8\nName=PhyPi Demo\nComment=PhyPi Configuration\nIcon=" > $HOME/Desktop/phypi_demo.desktop
+          printf $DIR >> $HOME/Desktop/phypi_demo.desktop
+          printf "/PhiPi_icon.png\nExec=lxterminal -t \"PhyPiDAQ\" -e \"" >> $HOME/Desktop/phypi_demo.desktop
+          printf "source ~/activate_phypi.sh; phypi.py " >> $HOME/Desktop/phypi_demo.desktop
+          printf $DIR >> $HOME/Desktop/phypi_demo.desktop
+          printf "/PhyPiDemo.daq\"\nTerminal=false\n" >> $HOME/Desktop/phypi_demo.desktop
+
+          # Ensure, that the desktop links have the correct permissions
+          chmod a+x $HOME/Desktop/*.desktop
+          break;;
+        [Nn]* ) echo "No installation of desktop icons "; break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+  done
 fi
 
 echo -e "\nThe full documentation (software, hardware and educational guidelines) can be found on GitHub:"
