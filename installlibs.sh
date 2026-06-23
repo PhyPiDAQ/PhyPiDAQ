bdaf88d0baa24db3a51cf90e3080057420d62685
Guenter Quast <guenter.quast@online.de>
2026-06-23 18:12:44 +0200

    installlibs.sh: support armhf and arm64

--- installlibs.sh
+++ installlibs.sh
@@ -67,10 +67,19 @@ while true; do
     read -p "Do you wish to install the PicoScope drivers? " yn
     case $yn in
         [Yy]* ) echo "Installing PicoScope drivers";
-	  sudo dpkg -i installlibs/picoscopelibs/*.deb; # picoscope
-	  sudo apt -f install # install all dependencies 	
-          sudo usermod -a -G tty $USER; # grant access to USB for the current user
-          break;;
+	  _arch=`uname -m`
+          if [[ "$_arch" == "aarch64" ]]; then
+	      sudo dpkg -i installlibs/picoscope64libs/*.deb; # picoscope for arm64
+	      sudo apt -f install # install all dependencies 	
+              sudo usermod -a -G tty $USER; # grant access to USB for the current user
+          elif [[ "$_arch" == "armhf" ]]; then
+	      sudo dpkg -i installlibs/picoscopelibs/*.deb; # picoscope for amrhf
+	      sudo apt -f install # install all dependencies 	
+              sudo usermod -a -G tty $USER; # grant access to USB for the current user
+	  else
+	      echo "!!! no libraries for architecture: $_arch"
+	  fi
+              break;;
         [Nn]* ) echo "Skipping PicoScope driver installation"; break;;
         * ) echo "Please answer yes or no.";;
     esac
